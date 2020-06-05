551da6020b873ab8b44cf15a60c5cc677320e35d
NazifaSadia <nazifas323@gmail.com>
2020-06-06 00:53:02 +0600

    Ema-John Simple project complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+
 const Cart = (props) => {
     const cart = props.cart;
     //const total = cart.reduce((total, prd) => total+prd.price, 0)
     let total = 0;
     for(let i = 0; i < cart.length; i++){
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
+        //debugger;
     }
 
     let shipping = 0;
@@ -22,8 +24,10 @@ const Cart = (props) => {
 
     //const tax = Math.round(total / 10);
     const tax = total / 10;
+    
     const grandTotal = (total + shipping + Number(tax)).toFixed(2);
-   const formatNumber = num =>{
+
+    const formatNumber = num =>{
        const precision = num.toFixed(2);
        return Number(precision);
    }
@@ -36,6 +40,10 @@ const Cart = (props) => {
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + VAT: {formatNumber(tax)}</small></p>
             <p>Total Price: {grandTotal}</p>
+            <br/>
+            {
+                props.children
+            }
         </div>
     );
 };
